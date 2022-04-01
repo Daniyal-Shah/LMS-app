8ae8c7c32956dffdb1f8a54a4554db549546d5c5
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-04-01 22:23:48 +0500

    Student can unenroll himself in the course

--- backend/src/routes/student_Courses.js
+++ backend/src/routes/student_Courses.js
@@ -71,4 +71,65 @@ router.post(
   }
 );
 
+router.post(
+  "/unenroll/:courseId",
+  passport.authenticate("student-rule", { session: false }),
+  async (req, res) => {
+    try {
+      const { enrollmentCode } = req.body;
+      if (
+        enrollmentCode &&
+        enrollmentCode.length >= 5 &&
+        enrollmentCode.length <= 15
+      ) {
+        const course = await Course.findOne({
+          enrollmentCode,
+          _id: req.params.courseId,
+        });
+
+        if (!course) return res.status(401).send("No such course found");
+
+        let alreadyEnrolled = false;
+        let index;
+
+        course.enrolledStudents.map((i, ind) => {
+          if ("" + i.studentId == "" + req.user._id) {
+            alreadyEnrolled = true;
+            index = ind;
+          }
+        });
+
+        if (!alreadyEnrolled) {
+          return res.status(401).send("You are not enrolled in this course");
+        }
+
+        course.enrolledStudents.splice(index, 1);
+
+        const student = await Student.findById({ _id: req.user._id });
+
+        student.courses.map((i, ind) => {
+          if ("" + i.courseId == "" + req.params.courseId) {
+            index = ind;
+          }
+        });
+
+        student.courses.splice(index, 1);
+
+        const result1 = await course.save();
+        const result2 = await student.save();
+
+        if (result1 && result2)
+          return res.send(`You are unenrolled in ${course.name} course`);
+
+        res.status(400).send("Something went wrong");
+      } else {
+        res
+          .status(401)
+          .send("Enrollment code must be a string having length (5 to 15");
+      }
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
 module.exports = router;
